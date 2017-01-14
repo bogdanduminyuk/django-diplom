e0765f53390dab5b4e44805c749c2b62f9d511dc
bogdanduminyuk <77934329bogdan@gmail.com>
2017-01-14 14:55:34 +0200

    Добавлена пустая страница generate_base_styles

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'generate_base_template', views.generate_base_template, name='generate_base_template')
+    url(r'generate_base_template', views.generate_base_template, name='generate_base_template'),
+    url(r'generate_base_styles', views.generate_base_styles, name='generate_base_styles'),
 ]
--- app/views.py
+++ app/views.py
@@ -13,10 +13,22 @@ def index(request):
 def generate_base_template(request):
     return render(request, 'app/generate_base_template.html', {
         'title': 'Генерация основного шаблона | ' + base_title,
-        'header': 'Генерация основного шаблона',
+        'page_header': 'Генерация основного шаблона',
         'panel_type': 'panel-primary',
         'submit_btn_type': 'btn-primary',
         'panel_heading': 'Данные для генерации',
         'form_action': '#',
         'submit_value': 'Адаптировать',
+    })
+
+
+def generate_base_styles(request):
+    return render(request, 'app/generate_base_styles.html', {
+        'title': 'Генерация шаблонных стилей | ' + base_title,
+        'page_header': 'Генерация шаблонных стилей',
+        'panel_heading': 'Заполните даные для генерации основных стилей',
+        'panel_type': 'panel-primary',
+        'submit_btn_type': 'btn-primary',
+        'form_action': '#',
+        'submit_value': 'Генерировать',
     })
