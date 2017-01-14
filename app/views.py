b0864ddaf86f113b2f61e8f5217264d25b05ade7
bogdanduminyuk <77934329bogdan@gmail.com>
2017-01-14 14:48:10 +0200

    Добавлен вывод полной формы generate_base_template.

--- app/views.py
+++ app/views.py
@@ -15,6 +15,8 @@ def generate_base_template(request):
         'title': 'Генерация основного шаблона | ' + base_title,
         'header': 'Генерация основного шаблона',
         'panel_type': 'panel-primary',
+        'submit_btn_type': 'btn-primary',
         'panel_heading': 'Данные для генерации',
         'form_action': '#',
+        'submit_value': 'Адаптировать',
     })
