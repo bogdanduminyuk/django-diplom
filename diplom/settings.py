714f8595ff99166a62c8b3d3213c1e7e7a43df96
bogdanduminyuk <77934329bogdan@gmail.com>
2017-01-14 16:32:49 +0200

    Добавлено новое django_app Test. В нем создан пример простейшей обработки формы.

--- diplom/settings.py
+++ diplom/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'test',
+    'app',
     'suit',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+
 ]
 
 MIDDLEWARE = [
