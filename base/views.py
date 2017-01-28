cda58bd70f3309bd12dfe2bcfdfdc95ef16e8527
bogdanduminyuk <77934329bogdan@gmail.com>
2017-01-28 11:33:01 +0200

    Добавлены параметры в view.home.

--- base/views.py
+++ base/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 
+base_title = 'Сервис помощи Веб-разработчику'
+
+
 def home(request):
-    return render(request, 'base/index.html', {})
+    return render(request, 'base/index.html', {
+        'title': base_title,
+        'header': 'Главная страница'
+    })
