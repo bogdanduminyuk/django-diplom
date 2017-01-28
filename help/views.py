b002600ac6f429b943c8be409235c343872433f1
bogdanduminyuk <77934329bogdan@gmail.com>
2017-01-28 12:24:54 +0200

    Изменены некоторые параметры представлениях справки.

--- help/views.py
+++ help/views.py
@@ -4,15 +4,25 @@ from django.shortcuts import render
 def common(request):
     return render(request, 'help/common_help.html', {
         'title': 'Справка по проекту',
-        'page_header': 'Справка по проекту',
+        'page_header': 'Справка по проекту (Наполнить контентом)',
         'panel_heading': 'Общая справка по данному проекту',
         'panel_type': 'panel-info',
     })
 
 
 def adaptation(request):
-    return render(request, 'help/common_help.html', {})
+    return render(request, 'help/common_help.html', {
+        'title': 'Справка по адаптации',
+        'page_header': 'Справка по адаптации (Наполнить контентом)',
+        'panel_heading': 'Общая справка по данному проекту',
+        'panel_type': 'panel-info',
+    })
 
 
 def generation(request):
-    return render(request, 'help/common_help.html', {})
+    return render(request, 'help/common_help.html', {
+        'title': 'Справка по генерации',
+        'page_header': 'Справка по генерации (Наполнить контентом)',
+        'panel_heading': 'Общая справка по данному проекту',
+        'panel_type': 'panel-info',
+    })
