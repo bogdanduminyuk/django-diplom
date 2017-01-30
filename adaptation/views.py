cf20546ca876abb257dead1fe61bfbbd2c4f1c23
bogdanduminyuk <77934329bogdan@gmail.com>
2017-01-30 14:40:23 +0200

    Удалены лишние комментарии.

--- adaptation/views.py
+++ adaptation/views.py
@@ -1,4 +1,3 @@
-import json
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -6,49 +5,10 @@ from .forms import WpAdaptForm
 
 
 def wordpress_adaptation(request):
-    """result = {
-        'title': 'Адаптация под WordPress',
-        'page_header': 'Адаптация под WordPress',
-        'panel_heading': 'Заполните данные для адаптации под WordPress',
-        'panel_type': 'panel-primary',
-        'submit_btn_type': 'btn-primary',
-        'submit_value': 'Адаптировать',
-        'form_action': '#',
-        'hidden': 'WordPress',
-    }
-
-    if request.method == 'POST':
-        form = WpAdaptForm(request.POST, request.FILES)
-
-        if form.is_valid():
-            filename = form.save_file()
-            form_data = request.POST.dict()
-            redirect = HttpResponseRedirect('/result')
-            request.session['filename'] = filename
-            request.session['form_data'] = form_data
-            return redirect
-        else:
-            print('form not valid')
-    else:
-        form = WpAdaptForm()
-
-    result['form'] = form
-    return render(request, 'base/form_common.html', result)"""
     return handle_adapt_request(request, WpAdaptForm, 'WordPress')
 
 
 def joomla_adaptation(request):
-    """
-    return render(request, 'base/form_common.html', {
-        'title': 'Адаптация под Joomla',
-        'page_header': 'Адаптация под Joomla',
-        'panel_heading': 'Заполните данные для адаптации под Joomla',
-        'panel_type': 'panel-primary',
-        'submit_btn_type': 'btn-primary',
-        'form_action': '#',
-        'submit_value': 'Адаптировать',
-    })
-    """
     return handle_adapt_request(request, WpAdaptForm, 'Joomla')
 
 
