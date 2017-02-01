1877521f907e75d0e5c9e4b18ab767f6be460bd0
bogdanduminyuk <77934329bogdan@gmail.com>
2017-02-01 14:32:51 +0200

    Добавлен файл functions. В нем добавлена функция split_path.

---
+++ adaptation/core/functions.py
@@ -0,0 +1,8 @@
+import os
+
+
+def split_path(path):
+    """Returns tuple of (folder, filename, ext) of given path"""
+    _dir, _file = os.path.split(path)
+    _filename, _ext = os.path.splitext(_file)
+    return _dir, _filename, _file
