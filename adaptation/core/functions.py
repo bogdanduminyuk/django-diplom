ebded85799f139db874a9f68ba6d2f97f68ead67
bogdanduminyuk <77934329bogdan@gmail.com>
2017-02-01 15:09:37 +0200

    Найден и устранен баг в functions.py split_text

--- adaptation/core/functions.py
+++ adaptation/core/functions.py
@@ -5,4 +5,4 @@ def split_path(path):
     """Returns tuple of (folder, filename, ext) of given path"""
     _dir, _file = os.path.split(path)
     _filename, _ext = os.path.splitext(_file)
-    return _dir, _filename, _file
+    return _dir, _filename, _ext
