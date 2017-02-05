09b7dee95cb6f7f3855f5df51ea91737becb77c8
bogdanduminyuk <77934329bogdan@gmail.com>
2017-02-05 15:59:25 +0200

    Добавлен docblock в wordpress.py/adaptaшon_wordpress. Добавлена заглушка check_version для вордпресс.

--- adaptation/core/wordpress.py
+++ adaptation/core/wordpress.py
@@ -3,12 +3,23 @@
 
 def adaptation_wordpress(src_dir, data):
     """
+    Global WordPress adaptation method.
 
-    :param src_dir:
-    :param data:
-    :return:
+    Realizes checking for version and calls the appropriate adaptation method.
+    Raises AdaptationVersionError if version can't be found.
+
+    :param src_dir: path to the dir of unpacked input file
+    :param data: data of input form and additional script data
+    :return: dict {filename : content} to create on files_layer
     """
-    # TODO: check for versions
-    return {
+    def check_version(version):
+        # TODO: check for versions
+        pass
+
+    check_version(1)
+
+    result = {
         "index.html": "<h1>wordpress</h1>"
     }
+
+    return result
