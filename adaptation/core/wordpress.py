80b4639fecc918ef62471c9e3022df938fb5f7ad
bogdanduminyuk <77934329bogdan@gmail.com>
2017-02-06 22:17:16 +0200

    Начата разработка адаптации стилей для wp.

--- adaptation/core/wordpress.py
+++ adaptation/core/wordpress.py
@@ -1,5 +1,8 @@
 # coding: utf-8
+import os
+
 from adaptation.core.classes import AdaptationVersionError
+from adaptation import settings as adapt_settings
 
 
 def adaptation_wordpress(src_dir, data):
@@ -33,8 +36,21 @@ def adaptation_wordpress_461(src_dir, data):
     :param data: data of input form and additional script data
     :return: dict {filename : content} to create on files_layer
     """
+    user_description = data['description']
 
-    styles = ""
+    name = data['name']
+    author = data.get('author', '')
+    description = data.get('description', '')
+    version = data['version']
+    theme_license = data.get('theme_license', '')
+    tags = data.get('tags', '')
+    comments = data.get('comments', '')
+
+    styles_css = os.path.join(src_dir, user_description['css'])
+    with open(styles_css, 'r', encoding='utf-8') as styles_file:
+        styles_content = styles_file.read()
+        # TODO: finish it
+        adapt_styles_content = adapt_settings.STYLES.format(name, author, description, version, theme_license, tags, comments, styles_content)
 
     result = {
         "index.html": "<h1>wordpress</h1>"
