468ba0341fb5f90b31f8a6c010be8f9e43dcd444
bogdanduminyuk <77934329bogdan@gmail.com>
2017-02-11 14:34:36 +0200

    Добавлен комментарий для get_wp_styles. Добавлена обработка стилей.

--- adaptation/WordPress/WordPressAdapter.py
+++ adaptation/WordPress/WordPressAdapter.py
@@ -8,13 +8,19 @@ class WordPressAdapter:
         self.data = data
 
     def adapt(self):
-        return {
-            "index.html": "<h1>HelloWorld</h1>",
-            "page.html": "<h2>Page</h2>",
-        }
+        src_css = self.description['css']
+        styles = self.get_wp_styles(src_css, self.data)
+        return {'style.css': styles}
 
     @staticmethod
     def get_wp_styles(source_css_file, data):
+        """
+        Gets content of input css remade to wp.
+
+        :param source_css_file: path to source css
+        :param data: form data
+        :return: wp css content
+        """
         with open(source_css_file, 'r', encoding='utf-8') as styles_file:
             return adapt_settings.STYLES.format(
                 data['name'],
