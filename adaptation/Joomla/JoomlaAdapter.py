a62a506a0d9ea02136fcd009152a08557c9bd77e
bogdanduminyuk <77934329bogdan@gmail.com>
2017-02-18 11:45:59 +0200

    Добавлены пустые классы JoomlaAdapter и JoomlaAdapter362.

---
+++ adaptation/Joomla/JoomlaAdapter.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+from adaptation.core.adapter import BaseAdapter
+
+
+class JoomlaAdapter(BaseAdapter):
+    pass
+
---
+++ adaptation/Joomla/JoomlaAdapter362.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+from adaptation.Joomla.JoomlaAdapter import JoomlaAdapter
+from adaptation import settings as adapt_settings
+
+
+class JoomlaAdapter362(JoomlaAdapter):
+    def adapt(self):
+        pass
