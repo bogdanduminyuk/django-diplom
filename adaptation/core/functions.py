7f0d4c3842d75abcfec8d9970e1b3930fb547b41
bogdanduminyuk <77934329bogdan@gmail.com>
2017-02-18 10:37:17 +0200

    Изменена функция split_path в functions.py

--- adaptation/core/functions.py
+++ adaptation/core/functions.py
@@ -4,5 +4,9 @@ import os
 def split_path(path):
     """Returns tuple of (folder, filename, ext) of given path"""
     _dir, _file = os.path.split(path)
-    _filename, _ext = os.path.splitext(_file)
+    if _dir == '':
+        _dir, _filename = _file, _dir
+        _ext = ''
+    else:
+        _filename, _ext = os.path.splitext(_file)
     return _dir, _filename, _ext
