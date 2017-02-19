2d14fe547a96b7bb905f824fbe1c508382ff4fa1
bogdanduminyuk <77934329bogdan@gmail.com>
2017-02-19 18:01:17 +0200

    Добавлена работа с xml в JoomlaAdapter

--- adaptation/Joomla/JoomlaAdapter.py
+++ adaptation/Joomla/JoomlaAdapter.py
@@ -1,12 +1,28 @@
 # coding: utf-8
-import os
+import xml.etree.ElementTree as ET
 
-from django.conf import settings
+from bs4 import BeautifulSoup as bs
 
-from adaptation.core.adapter import BaseAdapter
+from adaptation.core.BaseAdapter import BaseAdapter
 
 
 class JoomlaAdapter(BaseAdapter):
     """Base Joomla"""
-    pass
+    def __create_xml__(self):
+        extension = ET.Element('extension')
+        extension.set('version', '2.5')
+        extension.set('type', 'template')
+        extension.set('client', 'site')
+
+        # TODO: pretty print xml to file
+        for element, value in self.data.items():
+            sub_element = ET.SubElement(extension, element)
+            sub_element.text = value
+
+        xml_content = ET.tostring(extension, encoding='utf-8', method='xml').decode('utf-8')
+        return bs(xml_content, "xml").prettify()
+
+
+
+
 
