38b25812ac9c179d565038fab8875241a588332f
bogdanduminyuk <77934329bogdan@gmail.com>
2017-02-19 11:28:58 +0200

    Переработаны настройки wp. Добавлен index.php.tpl для wp.

--- adaptation/settings.py
+++ adaptation/settings.py
@@ -24,67 +24,51 @@ REQUIRED_DESCRIPTION_KEYS = [
 
 STATIC_CMS_ROOT = os.path.join(settings.BASE_DIR, 'adaptation', '{form}', 'static')
 
+
 # Handled CMS
 
 CMS = ['WordPress', 'Joomla']
 
 
-# Wordpress definition
+# definition of page parts
 
-WORDPRESS = {
-    # TODO: make wp_settings the same like joomla
-    "PREPARATION": {
-        "TEMPLATE_STRUCTURE": {
-            "css/style.css": "{content}",
-
-            "images/index.html": "<!DOCTYPE html><title></title>",
-
-            "js/index.html": "<!DOCTYPE html><title></title>",
-
-            "footer.php": "{content}",
-            "header.php": "{content}",
-            "index.php": "{content}",
-            "style.php": "{content}",
-        },
-
-        "RELATIVES": {
-            "get_template_directory_uri()": {
-                'href': ['link'],
-                'src': ['script', 'img'],
-            },
-        }
+PAGE_PARTS = {
+    "header": {
+        "SELECTOR": "#page-header",
     },
 
-    'INDEX': {
-        'HEADER': {
-            'SELECTOR': '#page-header',
-            'FILE': 'header.php',
-            'METHOD_CALL': 'get_header()',
-        },
-
-        'FOOTER': {
-            'SELECTOR': '#page-footer',
-            'FILE': 'footer.php',
-            'METHOD_CALL': 'get_footer()',
-        },
-    },
-
-    'STYLE': {
-        'SELECTOR': False,
-        'FILE': 'style.css',
-        'METHOD_CALL': False,
-        'CONTENT': "/*Theme Name: {0}\nAuthor: {1}\nDescription: {2}\nVersion: {3}\nLicense: {4}\nTags: {5}\n{6}\n*/\n\n{7}",
+    "footer": {
+        "SELECTOR": "#page-footer",
     },
 }
 
-JOOMLA = {
-    # key - description key, value - CMS folder name
-    "DIR_NAMES": {
-        "images": "images",
-        "css": "css",
-        "js": "js",
+
+# Wordpress definition
+
+WORDPRESS = {
+    "PREPARATION": {
+
+        "METHODS": {
+            "get_template_directory_uri()": {
+                # attr : tags of attr
+                "href": ['link'],
+                "src": ["script", "img"]
+            },
+        },
     },
 
+    "FILES": {
+        "footer.php": "{content}",
+        "header.php": "{content}",
+        "index.php": "{content}",
+        "style.php": "{content}",
+    },
+}
+
+
+# joomla definition
+
+JOOMLA = {
     "FILES": {
         # with dirs
         "css/index.html": "<!DOCTYPE html><title></title>",
