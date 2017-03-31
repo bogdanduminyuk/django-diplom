7141f101164cd02fe8c35677c04a5ed60c84d7ce
bogdanduminyuk <77934329bogdan@gmail.com>
2017-03-31 21:22:13 +0300

    Подправлен JoomlaAdapter362

--- adaptation/Joomla/JoomlaAdapter362.py
+++ adaptation/Joomla/JoomlaAdapter362.py
@@ -12,33 +12,33 @@ class JoomlaAdapter362(JoomlaAdapter):
     # TODO: fix $ is not a function
     def adapt(self, **kwargs):
         super(JoomlaAdapter362, self).adapt(**kwargs)
-        files = {}
+        # files = {}
 
         for filename, content in self.settings["FILES"].items():
             basename = os.path.basename(filename)
             file = self.theme.get_file(basename)
 
             if file is None:
-                if file is not 'templateDetails.xml':
-                    file = SimpleThemeFile("", os.path.join(self.theme.dst_dir, filename))
-                    file.content = content
-                    self.theme.add_file(file)
-            elif content == "{content}":
-                template_name = basename + ".tpl"
+                if content == "{content}":
+                    template_name = basename + ".tpl"
+                    if template_name in self.templates.keys():
+                        content = self.templates[template_name].get_content()
+                        # apply content
+                        # tpl_content = self.templates[template_name]["content"]
+                        # file_content = tpl_content.format(**self.format_data)
 
-                # apply data to content-template
-                if template_name in self.templates.keys():
-                    tpl_content = self.templates[template_name]["content"]
-                    file_content = tpl_content.format(**self.format_data)
+                file = SimpleThemeFile("", os.path.join(self.theme.dst_dir, filename))
+                file.content = content
+                self.theme.add_file(file)
             else:
                 file_content = content
 
-            files[filename] = file_content
+            # files[filename] = file_content
 
-        for filename in files.keys():
-            if os.path.basename(filename) == filename:
-                self.xml_file.add_child("files", "filename", filename)
+        #for filename in files.keys():
+        #    if os.path.basename(filename) == filename:
+        #        self.xml_file.add_child("files", "filename", filename)
 
-        files['templateDetails.xml'] = self.xml_file.prettify()
+        #files['templateDetails.xml'] = self.xml_file.prettify()
 
-        return files
+        # return files
