91c32646eb82f50c464ce0a718813b0e96c7988a
bogdanduminyuk <77934329bogdan@gmail.com>
2017-06-15 12:50:26 +0300

    Подправлена справка

--- help/views.py
+++ help/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def common_help(request):
     return render(request, 'help/common_help.html', {
         'title': 'Справка по проекту',
-        'page_header': 'Справка по проекту (Наполнить контентом)',
+        'page_header': 'Справка по проекту',
         'panel_heading': 'Общая справка по данному проекту',
         'panel_type': 'panel-info',
     })
