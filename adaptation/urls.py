c61b3202dc2ca8406989a4f39a070b1149c97ea1
bogdanduminyuk <77934329bogdan@gmail.com>
2017-06-15 04:28:33 +0300

    Добавлен URL для conflicts_test

--- adaptation/urls.py
+++ adaptation/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url, include
 from . import views
 
-
 urlpatterns = [
     url(r'wordpress_adaptation/$', views.wordpress_adaptation, name='wordpress_adaptation'),
     url(r'joomla_adaptation/$', views.joomla_adaptation, name='joomla_adaptation'),
+    url(r'conflicts/$', views.conflicts, name='conflicts'),
 
     url(r'wordpress_adaptation/test/$', views.wp_test, name='wp_test'),
     url(r'joomla_adaptation/test/$', views.joomla_test, name='joomla_test'),
-    url(r'conflicts/$', views.conflicts, name='conflicts')
+    url(r'conflicts/test/$', views.conflicts_test, name='conflicts_test')
 ]
+
 
